6deae38fd6de42d55261074e06aa772317b745a1
Renaud Guillard <dev@nore.fr>
2023-02-28 23:41:47 +0100

    Add DataSerializationManager::get<SerializerType>sFor () methods

--- src/Serialization/DataSerializationManager.php
+++ src/Serialization/DataSerializationManager.php
@@ -16,6 +16,7 @@ use NoreSources\Data\Serialization\Traits\DataFileMediaTypeNormalizerTrait;
 use NoreSources\Type\TypeDescription;
 use NoreSources\Data\Serialization\Traits\DataFileExtensionTrait;
 use NoreSources\Data\Serialization\Traits\DataFileUnserializerTrait;
+use NoreSources\Container\Container;
 
 /**
  * Data(De)serializer aggregate
@@ -99,17 +100,43 @@ class DataSerializationManager implements DataUnserializerInterface,
 		return false;
 	}
 
-	public function unserializeData($data,
+	/**
+	 * Get a list of DataUnserializerInterface capable of
+	 * unserialize the given data/media type
+	 *
+	 * @param unknown $data
+	 * @param MediaTypeInterface $mediaType
+	 * @return DataUnserializerInterface[]
+	 */
+	public function getDataUnserializerFor($data,
 		MediaTypeInterface $mediaType = null)
 	{
 		$stack = $this->stacks[DataUnserializerInterface::class];
-		foreach ($stack as $serializer)
+		return Container::filterValues($stack,
+			function ($s) use ($data, $mediaType) {
+				return $s->canUnserializeData($data, $mediaType);
+			});
+	}
+
+	public function unserializeData($data,
+		MediaTypeInterface $mediaType = null)
+	{
+		$list = $this->getDataUnserializerFor($data, $mediaType);
+		$messages = [];
+		foreach ($list as $serializer)
 		{
-			if (!$serializer->canUnserializeData($data, $mediaType))
-				continue;
-			return $serializer->unserializeData($data, $mediaType);
+			try
+			{
+				return $serializer->unserializeData($data, $mediaType);
+			}
+			catch (\Exception $e)
+			{
+				$messages[] = $e->getMessage();
+			}
 		}
-		throw new DataSerializationException();
+
+		throw new DataSerializationException(
+			\implode(PHP_EOL, $messages));
 	}
 
 	public function getSerializableDataMediaTypes()
@@ -144,20 +171,46 @@ class DataSerializationManager implements DataUnserializerInterface,
 		return false;
 	}
 
+	/**
+	 * Get a list of DataSerializerInterface capable of serialize the given data / media type
+	 *
+	 * @param unknown $data
+	 *        	Data to serialize
+	 * @param MediaTypeInterface $mediaType
+	 *        	Target media type
+	 * @return DataSerializerInterface[]
+	 */
+	public function getDataSerializersFor($data,
+		MediaTypeInterface $mediaType = null)
+	{
+		$stack = $this->stacks[DataSerializerInterface::class];
+		return Container::filterValues($stack,
+			function ($serializer) use ($data, $mediaType) {
+				return $serializer->canSerializeData($data, $mediaType);
+			});
+	}
+
 	public function serializeData($data,
 		MediaTypeInterface $mediaType = null)
 	{
 		if (!$mediaType && $data instanceof \Serializable)
 			return $data->serialize();
-		$stack = $this->stacks[DataSerializerInterface::class];
-		foreach ($stack as $serializer)
+		$list = $this->getDataSerializersFor($data, $mediaType);
+		$messages = [];
+		foreach ($list as $serializer)
 		{
-			if (!$serializer->canSerializeData($data, $mediaType))
-				continue;
-			return $serializer->serializeData($data, $mediaType);
+			try
+			{
+				return $serializer->serializeData($data, $mediaType);
+			}
+			catch (\Exception $e)
+			{
+				$messages[] = $e->getMessage();
+			}
 		}
 
-		throw new DataSerializationException();
+		throw new DataSerializationException(
+			\implode(PHP_EOL, $messages));
 	}
 
 	public function getUnserializableFileMediaTypes()
@@ -192,20 +245,56 @@ class DataSerializationManager implements DataUnserializerInterface,
 		return false;
 	}
 
+	/**
+	 * Get a list of DataFileUnserializerTrait capable of unserialize the given file of the given
+	 * media type
+	 *
+	 * @param unknown $filename
+	 *        	File to unserialize
+	 * @param MediaTypeInterface $mediaType
+	 *        	File media type
+	 * @param boolean $normalizeFileMediaType
+	 *        	Indicates if media type must be normalized
+	 * @return DataFileUnserializerTrait[]
+	 */
+	public function getDataFileUnserializersFor($filename,
+		MediaTypeInterface $mediaType = null,
+		$normalizeFileMediaType = true)
+	{
+		if ($normalizeFileMediaType)
+			$mediaType = $this->normalizeFileMediaType($filename,
+				$mediaType);
+		$stack = $this->stacks[DataFileUnerializerInterface::class];
+		return Container::filterValues($stack,
+			function ($serializer) use ($filename, $mediaType) {
+				return $serializer->canUnserializeFromFile($filename,
+					$mediaType);
+			});
+	}
+
 	public function unserializeFromFile($filename,
 		MediaTypeInterface $mediaType = null)
 	{
 		$mediaType = $this->normalizeFileMediaType($filename, $mediaType);
-		$stack = $this->stacks[DataFileUnerializerInterface::class];
-		foreach ($stack as $serializer)
+		$list = $this->getDataFileUnserializersFor($filename, $mediaType,
+			false);
+		$messages = [];
+		foreach ($list as $serializer)
 		{
-			if (!$serializer->canUnserializeFromFile($filename,
-				$mediaType))
-				continue;
-
-			return $serializer->unserializeFromFile($filename,
-				$mediaType);
+			try
+			{
+				return $serializer->unserializeFromFile($filename,
+					$mediaType);
+			}
+			catch (\Exception $e)
+			{
+				$messages[] = $e->getMessage();
+			}
 		}
+
+		if (\count($messages))
+			throw new DataSerializationException(
+				\implode(PHP_EOL, $messages));
 
 		$name = ($mediaType) ? \strval($mediaType) : \pathinfo(
 			$filename, PATHINFO_EXTENSION);
@@ -245,20 +334,57 @@ class DataSerializationManager implements DataUnserializerInterface,
 		return false;
 	}
 
+	/**
+	 * Get a list of DataFileSerializerInterface capable of serialize data to the given file to the
+	 * given file media type
+	 *
+	 * @param unknown $filename
+	 *        	Target file name
+	 * @param MediaTypeInterface $mediaType
+	 *        	Target media type
+	 * @param boolean $normalizeFileMediaType
+	 *        	Indicates if the media type must be normalized
+	 * @return DataFileSerializerInterface[]
+	 */
+	public function getDataFileSerializersFor($filename, $data = null,
+		MediaTypeInterface $mediaType = null,
+		$normalizeFileMediaType = true)
+	{
+		if ($normalizeFileMediaType)
+			$mediaType = $this->normalizeFileMediaType($filename,
+				$mediaType);
+		$stack = $this->stacks[DataFileSerializerInterface::class];
+		return Container::filterValues($stack,
+			function ($serializer) use ($filename, $data, $mediaType) {
+				return $serializer->canSerializeToFile($filename, $data,
+					$mediaType);
+			});
+	}
+
 	public function serializeToFile($filename, $data,
 		MediaTypeInterface $mediaType = null)
 	{
 		$mediaType = $this->normalizeFileMediaType($filename, $mediaType,
 			MediaTypeFactory::FROM_EXTENSION);
-		$stack = $this->stacks[DataFileSerializerInterface::class];
-		foreach ($stack as $serializer)
+		$list = $this->getDataFileSerializersFor($filename, $data,
+			$mediaType, false);
+		$messages = [];
+		foreach ($list as $serializer)
 		{
-			if (!$serializer->canSerializeToFile($filename, $data,
-				$mediaType))
-				continue;
-			return $serializer->serializeToFile($filename, $data,
-				$mediaType);
+			try
+			{
+				return $serializer->serializeToFile($filename, $data,
+					$mediaType);
+			}
+			catch (\Exception $e)
+			{
+				$messages[] = $e->getMessage();
+			}
 		}
+
+		if (\count($messages))
+			throw new DataSerializationException(
+				\implode(PHP_EOL, $messages));
 
 		$name = ($mediaType) ? \strval($mediaType) : \pathinfo(
 			$filename, PATHINFO_EXTENSION);
