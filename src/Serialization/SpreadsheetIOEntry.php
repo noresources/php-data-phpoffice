35b760064747906e54488bc209431027c58aa8d6
Renaud Guillard <dev@nore.fr>
2023-03-22 22:11:24 +0100

    PhpOffice spreadsheet support of ns-php-data
    * Use PHPExcel deprecated project to support PHP < 7.2
    * SpreadsheetSerializer
      * Implements File (un)serializer interfaces
      * Supports all built-in reader & writer or PHPExcel
    * Utility classes to help migration with PhpSpreadsheet
    
    Tests
    * Testing serialization of format producing constant results
    
    utitlity fix + add getHighestRowAndColumn

---
+++ src/Serialization/SpreadsheetIOEntry.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Copyright © 2023 by Renaud Guillard (dev@nore.fr)
+ * Distributed under the terms of the MIT License, see LICENSE
+ *
+ * #package Data
+ */
+namespace NoreSources\Data\PhpOffice\Serialization;
+
+use NoreSources\MediaType\MediaTypeInterface;
+use NoreSources\Bitset;
+
+class SpreadsheetIOEntry
+{
+
+	const READABLE = Bitset::BIT_01;
+
+	const WRITABLE = Bitset::BIT_02;
+
+	/**
+	 * Reader / Writer type
+	 *
+	 * @var string
+	 */
+	public $type;
+
+	/**
+	 * File format extension
+	 *
+	 * @var string
+	 */
+	public $extension;
+
+	/**
+	 * File format media type
+	 *
+	 * @var MediaTypeInterface|NULL
+	 */
+	public $mediaType;
+
+	/**
+	 * Property flags
+	 *
+	 * @var integer
+	 */
+	public $flags = 0;
+
+	public function __construct($type, $flags,
+		MediaTypeInterface $mediaType = null, $extension = null)
+	{
+		$this->type = $type;
+		$this->mediaType = $mediaType;
+		$this->flags = $flags;
+		$this->extension = ($extension) ? $extension : \strtolower(
+			$type);
+	}
+}
---
+++ src/Serialization/SpreadsheetIOFactory.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * Copyright © 2023 by Renaud Guillard (dev@nore.fr)
+ * Distributed under the terms of the MIT License, see LICENSE
+ *
+ * #package Data
+ */
+namespace NoreSources\Data\PhpOffice\Serialization;
+
+class SpreadsheetIOFactory
+{
+
+	public static function createWriter($spreadsheet, string $writerType)
+	{
+		return \call_user_func([
+			self::FACTORY_CLASS,
+			'createWriter'
+		], $spreadsheet, $writerType);
+		return $o;
+	}
+
+	public static function createReader(string $readerType)
+	{
+		return \call_user_func([
+			self::FACTORY_CLASS,
+			'createReader'
+		], $readerType);
+		return $io;
+	}
+
+	public static function createReaderForFile(string $filename)
+	{
+		return \call_user_func(
+			[
+				self::FACTORY_CLASS,
+				'createReaderForFile'
+			], $filename);
+		return $io;
+	}
+
+	public static function load(string $filename, int $flags = 0)
+	{}
+
+	public static function identify(string $filename)
+	{
+		return \call_user_func([
+			self::FACTORY_CLASS,
+			'identify'
+		], $filename);
+	}
+
+	public static function registerWriter(string $writerType,
+		string $writerClass)
+	{
+		return \call_user_func(
+			[
+				self::FACTORY_CLASS,
+				'registerWriter'
+			], $writerType, $writerClass);
+	}
+
+	public static function registerReader(string $readerType,
+		string $readerClass)
+	{
+		return \call_user_func(
+			[
+				self::FACTORY_CLASS,
+				'registerReader'
+			], $readerType, $readerClass);
+	}
+
+	const FACTORY_CLASS = \PHPExcel_IOFactory::class;
+}
